475204b81baaa68485c37e5b9a3f9a1118be2816
poludnev <poludnev.roman@gmail.com>
2021-04-07 21:48:55 +0300

    second step

--- src/init.js
+++ src/init.js
@@ -1,11 +1,14 @@
 import { program } from 'commander';
 
 const firstRun = () => {
-  program.description(
-    'Compares two configuration files and shows a difference.'
-  );
-  program.helpOption('-h, --help', 'read more information');
-  program.option('-V --version', 'output usage information');
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1>')
+    .arguments('<filepath2>')
+    .helpOption('-h, --help', 'read more information')
+    .option('-V --version', 'output usage information')
+    .option('-f --format [type]', 'output format');
   program.parse(process.argv);
 };
+
 export default firstRun;

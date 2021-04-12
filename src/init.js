ef163ddf0872b3e4fe1c0400ee753dde9a07b8a1
poludnev <poludnev.roman@gmail.com>
2021-04-13 00:03:51 +0300

    step 3 commit

--- src/init.js
+++ src/init.js
@@ -1,4 +1,6 @@
 import { program } from 'commander';
+import _ from 'lodash';
+import * as fs from 'fs';
 
 const firstRun = () => {
   program
@@ -7,7 +9,49 @@ const firstRun = () => {
     .arguments('<filepath2>')
     .helpOption('-h, --help', 'read more information')
     .option('-V --version', 'output usage information')
-    .option('-f --format [type]', 'output format');
+    .option('-f --format [type]', 'output format')
+    .action((file1, file2) => {
+      const f1 = fs.readFileSync(file1, 'utf-8');
+      const f2 = fs.readFileSync(file2, 'utf-8');
+
+      const parsedF1 = JSON.parse(f1);
+      const parsedF2 = JSON.parse(f2);
+      const f1Keys = Object.keys(parsedF1);
+      const f2Keys = Object.keys(parsedF2);
+      const keys = _.union(f1Keys, f2Keys).sort();
+
+      const fn = (val) => {
+        let status = 'equal';
+
+        if (!parsedF2[val]) {
+          status = 'deleted';
+        } else if (!parsedF1[val]) {
+          status = 'added';
+        } else if (parsedF2[val] !== parsedF1[val]) {
+          status = 'changed';
+        }
+        return [val, status];
+      };
+      const result = keys
+        .map(fn)
+        .reduce((acc, val) => {
+          if (val[1] == 'deleted') {
+            acc.push(`  - ${val[0]}: ${parsedF1[val[0]]}`);
+          }
+          if (val[1] === 'equal') {
+            acc.push(`    ${val[0]}: ${parsedF1[val[0]]}`);
+          }
+          if (val[1] === 'changed') {
+            acc.push(`  - ${val[0]}: ${parsedF1[val[0]]}`, `  + ${val[0]}: ${parsedF2[val[0]]}`);
+          }
+          if (val[1] === 'added') {
+            acc.push(`  + ${val[0]}: ${parsedF2[val[0]]}`);
+          }
+          return acc;
+        }, [])
+        .join('\n');
+      console.log(`{\n${result}\n}`);
+    });
   program.parse(process.argv);
 };
 

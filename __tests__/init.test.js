c3c979addde36384d739281a0100761410c938fd
poludnev <poludnev.roman@gmail.com>
2021-04-15 23:30:51 +0300

    test 5

--- __tests__/init.test.js
+++ __tests__/init.test.js
@@ -1,6 +1,11 @@
-import init from '../src/init.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+// import init from '../src/init.js';
 import { render } from '../src/init.js';
-import sum from '../sum.js';
+// import sum from '../sum.js';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 test('test1', () => {
   expect(render()).toBe('empty file path');

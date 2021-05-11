a28c8a5e37e9f893388fee2d7e25a8a0de7e2d0d
poludnev <poludnev.roman@gmail.com>
2021-05-12 00:03:09 +0300

    test try

--- src/parse.js
+++ src/parse.js
@@ -3,6 +3,7 @@ import * as path from 'path';
 import yaml from 'js-yaml';
 
 export default (file) => {
+  console.log('file', file);
   const fileExtName = path.extname(file);
   if (!fileExtName) throw new Error('Can not read the file extention');
   const currentDirectoryPath = process.cwd();

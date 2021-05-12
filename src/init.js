f2b451a3e30327e5bb855d08e6387ecbd36a1853
poludnev <poludnev.roman@gmail.com>
2021-05-13 00:18:11 +0300

    test run

--- src/init.js
+++ src/init.js
@@ -5,7 +5,8 @@ import diff from './diff.js';
 const program = new Command();
 
 export default (...argv) => {
-  console.log('test run0', argv);
+  console.log('test run0', argv.length);
+  // if (argv.length < 0)
   // console.log('test run1', process.argv);
   program
     .description('Compares two configuration files and shows a difference.')
@@ -16,13 +17,15 @@ export default (...argv) => {
     .option('-V --version', 'output usage information')
     .option('-f --format [type]', 'output format', 'stylish')
     .action((filepath1, filepath2, options) => {
-      // console.log('actoin', filepath1, filepath2);
+      console.log('actoin', filepath1, filepath2);
       if (filepath1.length === 0) throw new Error('empty file path');
       if (filepath2.length === 0) throw new Error('empty file path');
       const result = formatter[options.format](diff(filepath1, filepath2));
       console.log(result);
     });
-  program.parse(argv);
+  const args = argv.length <= 0 ? process.argv : argv;
+  // console.log(args);
+  program.parse(args);
 
   // console.log('test run2', process.argv);
 };

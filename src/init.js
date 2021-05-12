cf7bca2adc7cba75645bce2ebe166d336ced1c1b
poludnev <poludnev.roman@gmail.com>
2021-05-13 01:30:56 +0300

    test run

--- src/init.js
+++ src/init.js
@@ -30,7 +30,7 @@ export default (...args) => {
   // const args = argv.length <= 0 ? process.argv : argv;
   // console.log(args);
   program.parse([process.argv[0], process.argv[1], ...args]);
-  return program;
+  return program._actionResults;
 
   // console.log('test run2', process.argv);
 };

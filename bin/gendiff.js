23d6085026c847443814a5b2484c47059dd7ed1a
poludnev <poludnev.roman@gmail.com>
2021-05-15 00:29:11 +0300

    test run

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,49 @@
 #!/usr/bin/env node
-import firstRun from '../src/init.js';
+// import firstRun from '../src/init.js';
 
-firstRun();
+// firstRun();
+
+import pkg from 'commander';
+
+import formatter from './formatter.js';
+import diff from './diff.js';
+
+const { Command } = pkg;
+const program = new Command();
+
+export default () => {
+  // console.log('test run0', args);
+  // console.log('test run0', args.splice(-2));
+  // console.log('process', process.argv);
+  // console.log('test run2', [process.argv[0], process.argv[1], ...args]);
+  // if (argv.length < 0)
+  // console.log('test run1', process.argv);
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1> <filepath2>')
+    // .arguments('<filepath2>')
+    // .allowUnknownOption()
+    .helpOption('-h, --help', 'read more information')
+    .option('-V --version', 'output usage information')
+    .option('-f --format [type]', 'output format', 'stylish')
+    .action(formatter);
+  // (filepath1, filepath2, options) => {
+  // console.log('actoin', filepath1, filepath2, 'option', options, 'option end');
+  // const format = { format: options.format };
+  // if (filepath1.length === 0) throw new Error('empty file path');
+  // if (filepath2.length === 0) throw new Error('empty file path');
+  // if (!options.format) {
+  //   format.format = options;
+  // }
+  // const result = formatter[format.format](diff(filepath1, filepath2));
+  // console.log(result);
+  // return result;
+  // });
+  // const args = argv.length <= 0 ? process.argv : argv;
+  // console.log(args);
+  // program.parse([process.argv[0], process.argv[1], ...args, process.argv[1]]);
+
+  program.parse(process.argv);
+
+  // console.log('test run2', process.argv);
+};

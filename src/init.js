65d60a13c656d1fde701aaefc84342561ff048fb
poludnev <poludnev.roman@gmail.com>
2021-05-15 00:06:13 +0300

    test run

--- src/init.js
+++ src/init.js
@@ -6,11 +6,11 @@ import diff from './diff.js';
 const { Command } = pkg;
 const program = new Command();
 
-export default (...args) => {
-  console.log('test run0', args);
+export default () => {
+  // console.log('test run0', args);
   // console.log('test run0', args.splice(-2));
-  console.log('process', process.argv);
-  console.log('test run2', [process.argv[0], process.argv[1], ...args]);
+  // console.log('process', process.argv);
+  // console.log('test run2', [process.argv[0], process.argv[1], ...args]);
   // if (argv.length < 0)
   // console.log('test run1', process.argv);
   program
@@ -21,24 +21,24 @@ export default (...args) => {
     .helpOption('-h, --help', 'read more information')
     .option('-V --version', 'output usage information')
     .option('-f --format [type]', 'output format', 'stylish')
-    .action((filepath1, filepath2, options) => {
-      console.log('actoin', filepath1, filepath2, 'option', options, 'option end');
-      const format = { format: options.format };
-      if (filepath1.length === 0) throw new Error('empty file path');
-      if (filepath2.length === 0) throw new Error('empty file path');
-      if (!options.format) {
-        format.format = options;
-      }
-      const result = formatter[format.format](diff(filepath1, filepath2));
-      console.log(result);
-      return result;
-    });
+    .action(formatter);
+  // (filepath1, filepath2, options) => {
+  // console.log('actoin', filepath1, filepath2, 'option', options, 'option end');
+  // const format = { format: options.format };
+  // if (filepath1.length === 0) throw new Error('empty file path');
+  // if (filepath2.length === 0) throw new Error('empty file path');
+  // if (!options.format) {
+  //   format.format = options;
+  // }
+  // const result = formatter[format.format](diff(filepath1, filepath2));
+  // console.log(result);
+  // return result;
+  // });
   // const args = argv.length <= 0 ? process.argv : argv;
   // console.log(args);
   // program.parse([process.argv[0], process.argv[1], ...args, process.argv[1]]);
 
-  program.parse('[...args]');
-  return program._actionResults[0];
+  program.parse(process.argv);
 
   // console.log('test run2', process.argv);
 };

46c07ea89e9b5603f3ead7fd5b930d5a3a80610a
poludnev <poludnev.roman@gmail.com>
2021-05-15 13:12:47 +0300

    test run

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,15 +10,16 @@ test();
 
 // // firstRun();
 
-// import pkg from 'commander';
+import pkg from 'commander';
 // import cons from '../src/cons.js';
 // // import cons from '../src/cons.js';
 
 // import formatter from '../src/formatter.js';
+// formatter(fil)
 // // // import diff from './diff.js';
 
-// const { Command } = pkg;
-// const program = new Command();
+const { Command } = pkg;
+const program = new Command();
 // // // console.log('gendiff 123123123');
 // const test = (file1, file2, option) => {
 //   //   console.log('test start bin');
@@ -28,19 +29,19 @@ test();
 //   //   // console.log('test run2', [process.argv[0], process.argv[1], ...args]);
 //   //   // if (argv.length < 0)
 //   //   // console.log('test run1', process.argv);
-//   program
-//     .description('Compares two configuration files and shows a difference.')
-//     .arguments('<filepath1> <filepath2>')
-//     // .arguments('<filepath2>')
-//     // .allowUnknownOption()
-//     .helpOption('-h, --help', 'read more information')
-//     .option('-V --version', 'output usage information')
-//     .option('-f --format [type]', 'output format', 'stylish')
-//     .action((file1, file2, option) => {
-//       console.log('one another test');
-//       console.log(formatter(file1, file2, option));
-//       return formatter(file1, file2, option);
-//     });
+program
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  // .arguments('<filepath2>')
+  //     // .allowUnknownOption()
+  //     .helpOption('-h, --help', 'read more information')
+  .option('-V --version', 'output usage information')
+  .option('-f --format [type]', 'output format', 'stylish')
+  .action((file1, file2, option) => {
+    console.log('one another test');
+    //       console.log(formatter(file1, file2, option));
+    //       return formatter(file1, file2, option);
+  });
 //   //   // (filepath1, filepath2, options) => {
 //   //   // console.log('actoin', filepath1, filepath2, 'option', options, 'option end');
 //   //   // const format = { format: options.format };
@@ -57,7 +58,7 @@ test();
 //   //   // console.log(args);
 //   //   // program.parse([process.argv[0], process.argv[1], ...args, process.argv[1]]);
 //   //   console.log('test start2 bin');
-//   //   program.parse(process.argv);
+program.parse(process.argv);
 //   //   console.log('test start3 bin');
 //   //   return formatter(file1, file2, option);
 

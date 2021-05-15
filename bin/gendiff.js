7209404e3daddea2379b644471188faccc901c5b
poludnev <poludnev.roman@gmail.com>
2021-05-15 12:44:37 +0300

    test run

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
-import firstRun from '../src/init.js';
+// import firstRun from '../src/init.js';
 
-firstRun();
+// firstRun();
 
-// import pkg from 'commander';
+import pkg from 'commander';
 // import cons from '../src/cons.js';
 
-// import formatter from '../src/formatter.js';
+import formatter from '../src/formatter.js';
 // // import diff from './diff.js';
 
-// const { Command } = pkg;
-// const program = new Command();
+const { Command } = pkg;
+const program = new Command();
 // // console.log('gendiff 123123123');
 // export default (file1, file2, option) => {
 //   console.log('test start bin');
@@ -20,18 +20,18 @@ firstRun();
 //   // console.log('test run2', [process.argv[0], process.argv[1], ...args]);
 //   // if (argv.length < 0)
 //   // console.log('test run1', process.argv);
-//   program
-//     .description('Compares two configuration files and shows a difference.')
-//     .arguments('<filepath1> <filepath2>')
-//     // .arguments('<filepath2>')
-//     // .allowUnknownOption()
-//     .helpOption('-h, --help', 'read more information')
-//     .option('-V --version', 'output usage information')
-//     .option('-f --format [type]', 'output format', 'stylish')
-//     .action((file1, file2, option) => {
-//       console.log('one another test');
-//       return formatter(file1, file2, option);
-//     });
+program
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  // .arguments('<filepath2>')
+  // .allowUnknownOption()
+  .helpOption('-h, --help', 'read more information')
+  .option('-V --version', 'output usage information')
+  .option('-f --format [type]', 'output format', 'stylish')
+  .action((file1, file2, option) => {
+    console.log('one another test');
+    return formatter(file1, file2, option);
+  });
 //   // (filepath1, filepath2, options) => {
 //   // console.log('actoin', filepath1, filepath2, 'option', options, 'option end');
 //   // const format = { format: options.format };
@@ -52,5 +52,5 @@ firstRun();
 //   console.log('test start3 bin');
 //   return formatter(file1, file2, option);
 
-//   // console.log('test run2', process.argv);
+//   console.log('test run2', process.argv);
 // };

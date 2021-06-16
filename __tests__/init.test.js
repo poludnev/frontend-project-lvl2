7871fbe7134cebd141587cf43a5957e7474f38a8
poludnev <poludnev.roman@gmail.com>
2021-06-16 19:33:15 +0300

    fix file reading: move readingto test block

--- __tests__/init.test.js
+++ __tests__/init.test.js
@@ -1,21 +1,17 @@
 import path from 'path';
-import * as fs from 'fs';
 import { fileURLToPath } from 'url';
 
 import { expect, test } from '@jest/globals';
-import formatter from '../src/genDiff.js';
+import readFile from '../src/fileReader.js';
+import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const stylishResult = fs.readFileSync(getFixturePath('results/stylish.txt'), 'utf-8');
-const plainResult = fs.readFileSync(getFixturePath('results/plain.txt'), 'utf-8');
-const jsonResult = fs.readFileSync(getFixturePath('results/json.txt'), 'utf-8');
-
-const jsonFilePath2 = getFixturePath('config2.json');
 const jsonFilePath1 = getFixturePath('config1.json');
+const jsonFilePath2 = getFixturePath('config2.json');
 const yamlFilePath1 = getFixturePath('config1.yml');
 const yamlFilePath2 = getFixturePath('config2.yaml');
 const unsupportedFilePath = getFixturePath('unsupportedConfig.txt');
@@ -27,22 +23,23 @@ const argsTofail = [
   [jsonFilePath1, unsupportedFilePath],
 ];
 
-test.each(argsTofail)('name0', (args) => {
-  expect(() => formatter(...args)).toThrow();
+test.each(argsTofail)('to-fail-case #%#', (args) => {
+  expect(() => genDiff(...args)).toThrow();
 });
 
 const argsToPass = [
   {
-    args: [jsonFilePath1, jsonFilePath2], expected: stylishResult,
+    args: [jsonFilePath1, jsonFilePath2], expectedPath: 'results/stylish.txt',
   },
   {
-    args: [yamlFilePath1, yamlFilePath2, 'plain'], expected: plainResult,
+    args: [yamlFilePath1, yamlFilePath2, 'plain'], expectedPath: 'results/plain.txt',
   },
   {
-    args: [yamlFilePath1, jsonFilePath2, 'json'], expected: jsonResult,
+    args: [yamlFilePath1, jsonFilePath2, 'json'], expectedPath: 'results/json.txt',
   },
 ];
 
-test.each(argsToPass)('name', ({ args, expected }) => {
-  expect(formatter(...args)).toEqual(expected);
+test.each(argsToPass)('to-pass-case #%#', ({ args, expectedPath }) => {
+  const expected = readFile(getFixturePath(expectedPath));
+  expect(genDiff(...args)).toEqual(expected);
 });

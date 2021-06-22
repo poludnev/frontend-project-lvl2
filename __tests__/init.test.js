f4d603bed2fc4253b6f2e38e35c94b4ea1dd88fb
poludnev <poludnev.roman@gmail.com>
2021-06-23 02:01:00 +0300

    fixed tests

--- __tests__/init.test.js
+++ __tests__/init.test.js
@@ -1,20 +1,12 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 import { expect, test } from '@jest/globals';
-import readFile from '../src/fileReader.js';
+import { readFile } from '../src/utils.js';
 import genDiff from '../src/genDiff.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-const jsonFilePath1 = getFixturePath('config1.json');
-const jsonFilePath2 = getFixturePath('config2.json');
-const yamlFilePath1 = getFixturePath('config1.yml');
-const yamlFilePath2 = getFixturePath('config2.yaml');
-const unsupportedFilePath = getFixturePath('unsupportedConfig.txt');
+const jsonFilePath1 = ('__fixtures__/config1.json');
+const jsonFilePath2 = ('__fixtures__/config2.json');
+const yamlFilePath1 = ('__fixtures__/config1.yml');
+const yamlFilePath2 = ('__fixtures__/config2.yaml');
+const unsupportedFilePath = ('__fixtures__/unsupportedConfig.txt');
 
 const argsTofail = [
   [jsonFilePath1, ''],
@@ -29,17 +21,17 @@ test.each(argsTofail)('to-fail-case #%#', (args) => {
 
 const argsToPass = [
   {
-    args: [jsonFilePath1, jsonFilePath2], expectedPath: 'results/stylish.txt',
+    args: [jsonFilePath1, jsonFilePath2], expectedPath: '__fixtures__/results/stylish.txt',
   },
   {
-    args: [yamlFilePath1, yamlFilePath2, 'plain'], expectedPath: 'results/plain.txt',
+    args: [yamlFilePath1, yamlFilePath2, 'plain'], expectedPath: '__fixtures__/results/plain.txt',
   },
   {
-    args: [yamlFilePath1, jsonFilePath2, 'json'], expectedPath: 'results/json.txt',
+    args: [yamlFilePath1, jsonFilePath2, 'json'], expectedPath: '__fixtures__/results/json.txt',
   },
 ];
 
-test.each(argsToPass)('to-pass-case #%#', ({ args, expectedPath }) => {
-  const expected = readFile(getFixturePath(expectedPath));
+test.each(argsToPass)('to-pass-case #%# $expectedPath', ({ args, expectedPath }) => {
+  const expected = readFile(expectedPath);
   expect(genDiff(...args)).toEqual(expected);
 });
